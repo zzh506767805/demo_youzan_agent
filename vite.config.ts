69833a5cc18546c4bd25fed3c29fa37b13749da0
zzh506767805 <506767805@qq.com>
2025-02-11 15:04:16 +0800

    fix: resolve production deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     AutoImport({
@@ -40,7 +42,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 }) 
